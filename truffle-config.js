d554ec579889d4027f7c6b5c55d1ae6c3ae8799c
Pragna M <millp01@pfw.edu>
2022-10-23 19:32:53 -0400

    deployment added

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,4 @@
-const HDWalletProvider = require("@truffle/hdwallet-provider")
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 const mnemonic = require("./secrets.json").mnemonic;
 
 module.exports = {
@@ -7,8 +7,8 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*", // Match any network id
-      gas: 5000000
-    }
+      gas: 5000000,
+    },
   },
   compilers: {
     solc: {
@@ -16,9 +16,30 @@ module.exports = {
       settings: {
         optimizer: {
           enabled: true, // Default: false
-          runs: 200      // Default: 200
+          runs: 200, // Default: 200
         },
-      }
-    }
-  }
+      },
+    },
+  },
+  chainstack: {
+    deployment: {
+      accounts: [
+        {
+          privateKeyFile:
+            "//root/.ethereum/keystore/UTC--2019-08-01T07-24-17.754471456Z--73236c8d8aaee5263e8a32c71171030dd7a3e8e6",
+          password: "chainstack1234!",
+        },
+      ],
+      host: "ENDPOINT",
+      port: false,
+      protocol: "https",
+      type: "rpc",
+    },
+    dappConnection: [
+      "$WEB3", // uses pre existing web3 object if available (e.g in Mist)
+      "ws://localhost:8546",
+      "http://localhost:8545",
+    ],
+    gas: "auto",
+  },
 };
